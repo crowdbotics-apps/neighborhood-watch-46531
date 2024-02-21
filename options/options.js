ad96512b696bef60b99912ceb4fe634442ceff5c
Crowdbotics <team@crowdbotics.com>
2024-02-21 15:09:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "neighborhood-watch-46531",
+  url: "https://neighborhood-watch-46531.botics.co",
+  api: "https://neighborhood-watch-46531.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
